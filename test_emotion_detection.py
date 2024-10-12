93ac5682554eabc9c81487fd8773ca44290003ea
fareena16 <mohamedraizudeen1@gmail.com>
2024-10-12 11:20:02 +0530

    Create test_emotion_detection.py

---
+++ test_emotion_detection.py
@@ -0,0 +1,6 @@
+Statement	Dominant Emotion
+I am glad this happened	joy
+I am really mad about this	anger
+I feel disgusted just hearing about this	disgust
+I am so sad about this	sadness
+I am really afraid that this will happen	fear
